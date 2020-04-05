00a702db02c2394b88549b0ad94298c49f4d8651
hoainp <hoaibang07@github.com>
2020-04-05 15:25:40 +0700

    Init magento 2 with docker

---
+++ public_html/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>This is docker demo</title>
+    </head>
+    <body>
+        <h1>Hello Cloudreach!</h1>
+        <h4>Attempting MySQL connection from php...</h4>
+        <?php
+        $host = 'mysql';
+        $user = 'root';
+        $pass = 'rootpassword';
+        $conn = new mysqli($host, $user, $pass);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        echo "Connected to MySQL successfully!";
+        ?>
+    </body>
+</html>
